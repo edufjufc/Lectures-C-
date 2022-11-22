d2da750802945ebe627828ebf2b5458a86ba56f5
Nikol <nikol.klimova.2018@mail.ru>
2022-11-22 16:28:26 +0300

    Lesson 4

---
+++ Lesson_4/Program.cs
@@ -0,0 +1,13 @@
+﻿int FindSum(int number)
+{
+    int sum=0;
+
+    for(int curret=1; curret<=number; curret++) sum+=curret;
+
+    return sum;
+}
+
+Console.Write("Input a number: ");
+int a=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Sum of number between 1 and {a} is {FindSum(a)}");
